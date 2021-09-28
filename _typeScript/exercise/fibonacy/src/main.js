d1324d039ca5f4b61ae2720ef1b0d7809c273b95
nhatlinh8816 <dangnhatlinh0213@gmail.com>
2021-09-28 21:10:27 +0700

    commit_28.9

---
+++ _typeScript/exercise/fibonacy/src/main.js
@@ -0,0 +1,13 @@
+function sumFibonacy(x) {
+    var fibo = [0, 1];
+    if (x <= 0) {
+        return 0;
+    }
+    var sum = fibo[0] + fibo[1];
+    for (var i = 2; i <= x; i++) {
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
+        sum += fibo[i];
+    }
+    return sum;
+}
+console.log("Sum of 3 Fibonacy number first is " + sumFibonacy(4));
