67bae8921cf0931bf3027c4c87efb702ed6c1fc4
nhatlinh8816 <dangnhatlinh0213@gmail.com>
2021-09-29 22:45:12 +0700

    commit_29.9

---
+++ _angular_overview/exercise/angular-calculator-app/src/app/calculator/calculator.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+export interface ICaculator {
+  firstNum: number;
+  secondNum: number;
+  operator: string;
+}
+
+
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+  caculator: ICaculator = {
+    firstNum: null,
+    secondNum: null,
+    operator: 'add',
+
+  };
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getFirstNum(value: any) {
+    this.caculator.firstNum = value;
+  }
+
+  getSecondNum(value: any) {
+    this.caculator.secondNum = value;
+  }
+
+  getOperator(value: any) {
+    this.caculator.operator = value;
+  }
+  getResult(firstNum: number, secondNum: number, operator: string){
+    this.caculator.firstNum = firstNum;
+    this.caculator.secondNum = secondNum;
+    this.caculator.operator = operator;
+    switch (operator) {
+      case 'add':
+        return firstNum + secondNum;
+        break;
+      case 'sub':
+        return firstNum - secondNum;
+        break;
+      case 'multi':
+        return firstNum * secondNum;
+        break;
+      case 'divide':
+        return firstNum / secondNum;
+        break;
+    }
+  }
+}
